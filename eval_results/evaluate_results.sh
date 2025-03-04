36b3a1067703a8d8716a714aa1c21aa43d62fd1e
Maria Dancianu <mary_0094@hotmail.it>
2025-03-04 15:34:51 +0100

    move rag code from nb to python script and add bash script to automatically compute eval

---
+++ eval_results/evaluate_results.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+FOLDER_PATH="/Users/mariadancianu/Desktop/Git Projects/SQuAD_RAG_experiments/eval_results"
+
+for file in "$FOLDER_PATH"/pred_500*.json; do
+    if [[ -f "$file" ]]; then
+        out_file="${FOLDER_PATH}/eval_$(basename "$file")"
+        echo "Processing: $file -> Output: $out_file"	
+        python evaluation.py data_updated_500.json "$file" --out-file "$out_file"
+
+    fi
+done
+
